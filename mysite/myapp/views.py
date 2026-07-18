ad6e7867d65e68ff5d648f4481c41bea567d0872
natashaL2191 <linaresn2024@gmail.com>
2026-07-17 20:59:57 -0500

    added view

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from re import requests
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, world!")
